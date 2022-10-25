4e7564d8d7f7e76a3d66b5a27a4b1f224e1a0ef8
hseiji <henrique.hst@gmail.com>
2022-10-24 22:18:12 -0400

    add functions

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardComponent implements OnInit {
 
-  squares: any[];
-  xIsNext: boolean;
-  winner: string;
+  squares!: any[];
+  xIsNext!: boolean;
+  winner!: string;
 
   constructor() { }
 
--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-square',
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class SquareComponent {
 
-  somenum = Math.random();
+  @Input()
+  value!: 'X' | 'O';
 
 }
